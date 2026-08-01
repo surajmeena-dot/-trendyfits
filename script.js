c1927dbe6fd58b691a58ca8a2be10e2bdf5ebf31
surajmeena-dot <suraj_meena@icloud.com>
2026-08-01 13:37:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const products=[
+{name:'Buda Jeans Polo',price:'₹310',link:'https://ajiio.in/8CEqpia'},
+{name:'John Players Shirt',price:'₹618',link:'https://ajiio.in/D8JLAG7'}
+];
+const wrap=document.getElementById('products');
+products.forEach(p=>{
+wrap.innerHTML+=`<div class="card"><h3>${p.name}</h3><p>${p.price}</p><a class="btn" href="${p.link}">Shop Now</a></div>`;
+});
